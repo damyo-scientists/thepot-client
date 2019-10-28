d4de42eebc159568a4d020792c62c87657b5b850
Redrock912 <leset912@gmail.com>
2019-10-28 18:28:27 +0900

    국밥드실분

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import Description from "./components/Description";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    let gookbabCount = parseInt(this.state.value, 10);
+
+    let converted = gookbabCount / 5000;
+    alert("고것은 " + converted + " 국밥이네요.");
+
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            가격:
+            <input
+              type="text"
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+          </label>
+          <input type="submit" class="btnClass" value="확인" />
+        </form>
+        <Description />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Description.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Description extends Component {
+  render() {
+    return <h2>당신의 돈 국밥으로 환산해보셨습니까?</h2>;
+  }
+}
+
+export default Description;
