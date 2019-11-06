b04b6398a84d6b0ba2af5daecbe0bdb83f822353
Redrock912 <leset912@gmail.com>
2019-11-06 20:01:23 +0900

    다시 바꿨습니다.

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,18 +1,31 @@
-import React, {Component} from "react";
-import {inject, observer} from "mobx-react";
+import React, { Component } from "react";
+import { inject, observer } from "mobx-react";
+import Conversion from "../components/Conversion";
 
-@inject('userInput')
+@inject("userInput")
 @observer
 class Description extends Component {
   constructor(props) {
     super(props);
-    console.log(props);
   }
 
   render() {
-    const {userInput} = this.props;
-    return (<div><h2>당신의 돈 국밥으로 환산해보셨습니까?</h2>
-      <h3>당신이 알고싶은 국밥가 : {userInput.price}</h3></div>);
+    const { userInput } = this.props;
+    let gb = new Conversion(userInput.price);
+    return (
+      <div>
+        <h2>당신의 돈 국밥으로 환산해보셨습니까?</h2>
+        <h3>당신이 알고싶은 국밥 갯수 : {gb.ToGookbab()}</h3>
+        <h3>
+          고것은{" "}
+          <font size="40" color="yellow">
+            {" "}
+            {gb.ToChicken()}
+          </font>{" "}
+          치킨이구연
+        </h3>
+      </div>
+    );
   }
 }
 
