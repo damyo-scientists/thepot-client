4f52dc2f489dd04e6ce1869f31adb51fb1670153
Redrock912 <leset912@gmail.com>
2019-11-06 19:36:42 +0900

    Revert "치킨 가격 , 국밥가격 페이지에 표시, 확인버튼은 필요가 없어져서 리셋버튼으로 바꿨습니다."

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
-import { inject, observer } from "mobx-react";
+import React, {Component} from "react";
+import {inject, observer} from "mobx-react";
 
-@inject("userInput")
+@inject('userInput')
 @observer
 class Description extends Component {
   constructor(props) {
@@ -10,23 +10,9 @@ class Description extends Component {
   }
 
   render() {
-    const { userInput } = this.props;
-    let gookbab = userInput.price / 5000;
-    let chicken = userInput.price / 16000;
-    return (
-      <div>
-        <h2>당신의 돈 국밥으로 환산해보셨습니까?</h2>
-        <h3>당신이 알고싶은 국밥가 : {gookbab}</h3>
-        <h3>
-          고것은{" "}
-          <font size="40" color="yellow">
-            {" "}
-            {chicken}{" "}
-          </font>{" "}
-          치킨이구연{" "}
-        </h3>
-      </div>
-    );
+    const {userInput} = this.props;
+    return (<div><h2>당신의 돈 국밥으로 환산해보셨습니까?</h2>
+      <h3>당신이 알고싶은 국밥가 : {userInput.price}</h3></div>);
   }
 }
 
